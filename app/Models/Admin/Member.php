a52842b1c819399a22b652f4e88e9deba1fcc527
programmerPatient <494429797@qq.com>
2019-09-16 20:49:27 +0800

    ss

--- app/Models/Admin/Member.php
+++ app/Models/Admin/Member.php
@@ -8,6 +8,8 @@ use Illuminate\Auth\Authenticatable;
 class Member extends Model implements \Illuminate\Contracts\Auth\Authenticatable
 {
     protected $table = 'member';
+        //使用trait，就相当于将trait代码段复制到这个位置
+    use Authenticatable;
 
     protected $fillable =['username','password','gender','email','mobile','tableau_id','created_at','status'];
 }
