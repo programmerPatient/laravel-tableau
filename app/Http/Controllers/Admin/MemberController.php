85dc1770587764ae0472bc18fe63125b6e615314
programmerPatient <494429797@qq.com>
2019-09-16 08:10:27 +0800

    s

--- app/Http/Controllers/Admin/MemberController.php
+++ app/Http/Controllers/Admin/MemberController.php
@@ -63,6 +63,7 @@ class MemberController extends Controller
 
     //四修改会员信息
     public function modify($id){
+         return view('admin.member.modify');
         $data = Member::where('id',$id)->get()->first();
         if(Input::method() == 'POST'){
             $post = Input::only(['username','password','gender','type','status','email','mobile']);
