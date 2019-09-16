fddb77e6331bcc9031fd44e389ed4123a727262d
programmerPatient <494429797@qq.com>
2019-09-16 20:24:55 +0800

    ss

--- app/Http/Controllers/Admin/PublicController.php
+++ app/Http/Controllers/Admin/PublicController.php
@@ -82,7 +82,7 @@ class PublicController extends Controller
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "{\"credentials\":{\"name\":\"" . $admin['username'] . "\",\"password\":\"admin\",\"site\":{\"contentUrl\":\"\"}}}",
+            CURLOPT_POSTFIELDS => "{\"credentials\":{\"name\":\"" . $admin->username . "\",\"password\":\"admin\",\"site\":{\"contentUrl\":\"\"}}}",
             CURLOPT_HTTPHEADER => array(
                 "User-Agent: TabCommunicate",
                 "Content-Type: application/json",
